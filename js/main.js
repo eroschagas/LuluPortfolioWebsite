b5ef90a12b3b9ef999c1f22c648f1d2a824a139e
Eros Chagas <eroschagas@hotmail.com>
2022-03-24 17:20:53 -0300

    remove address bar

--- js/main.js
+++ js/main.js
@@ -134,3 +134,9 @@ if (window.DeviceOrientationEvent) {
     false
   );
 }
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    // This hides the address bar:
+    window.scrollTo(0, 1);
+  }, 0);
+});
